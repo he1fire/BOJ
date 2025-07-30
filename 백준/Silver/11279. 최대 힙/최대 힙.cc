74b1b363b00c82f61126e4e5ff88ccdc4c88f93a
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:43:46 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main () {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    priority_queue<int> pQ;
+    int N;
+    cin >> N;
+    for (int i=0;i<N;i++)
+        {
+        int a;
+        cin >> a;
+        if (a!=0)
+        pQ.push(a);
+        else
+            {
+            if (!pQ.empty())
+                {
+                cout << pQ.top() << "\n";
+                pQ.pop();
+                }
+            else
+            cout << "0\n";
+            }
+        }
+    return 0;
+    }
