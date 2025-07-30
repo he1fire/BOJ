a7e84076202886ae63fd4b03117d500b51d2720c
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:33:02 +0900

    [Silver III] Title: N과 M (7), Time: 496 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+vector<int> v, arr;
+int N, M;
+void backtrack(int x, int cost){
+    if (cost==M){
+        for (int i=0;i<M;i++)
+            cout << v[i] << " ";
+        cout << "\n";
+        return;
+    }
+    for (int i=0;i<N;i++){
+        v.push_back(arr[i]);
+        backtrack(i,cost+1);
+        v.pop_back();
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i=0;i<N;i++){
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    sort(arr.begin(),arr.end());
+    backtrack(0,0);
+    return 0;
+}
