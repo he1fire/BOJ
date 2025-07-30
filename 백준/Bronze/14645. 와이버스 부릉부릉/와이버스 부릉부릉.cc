57e3b3704f1acce772e61e2824758ae8cecd4059
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:23:17 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int N, M, a, b;
+    cin >> N >> M;
+    for (int i=1;i<=N;i++)
+    cin >> a >> b;
+    cout << "비와이";
+    return 0;
+    }
