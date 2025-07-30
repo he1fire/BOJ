0df65f8298c2a380b2985d24fa2057c0243e470b
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 14:05:59 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+typedef pair<ll,ll> pll;
+ll N, M, T;
+int main(){
+    scanf("%lld + %lld = %lld", &N, &M, &T);
+    N+M==T ? cout << "YES" : cout << "NO";
+    return 0;
+}
