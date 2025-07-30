2fb68e9e8fd605b98e98f9759892d2eb3f3b970b
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:46:18 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie();
+    cin >> N >> M;
+    cout << N*N*M;
+    return 0;
+}
