bc062d6780f9b069b430cc27cb9e06da708838f7
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:18:53 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    long long N;
+    cin >> N;
+    if (N%5==0 || N%5==2)
+        {
+        cout << "CY";
+        }
+    else 
+        {
+        cout << "SK";
+        }
+    return 0;
+    }
