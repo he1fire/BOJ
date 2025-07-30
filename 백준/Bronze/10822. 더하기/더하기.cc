1d680f37d1b028c39e2b819985dcdfe4af28f701
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:32:00 +0900

    [Bronze II] Title: 더하기, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    int ans=0;
+    string N, x;
+    cin >> N;
+    N.push_back(',');
+    for (int i=0;i<N.size();i++){
+        if (N[i]!=',')
+            x.push_back(N[i]);
+        else{
+            int cnt=0;
+            reverse(x.begin(),x.end());
+            for (int j=0, a=1;j<x.size();j++, a*=10){
+                cnt+=(x[j]-48)*a;
+            }
+            ans+=cnt;
+            x.clear();
+        }
+    }
+    cout << ans;
+    return 0;
+}
