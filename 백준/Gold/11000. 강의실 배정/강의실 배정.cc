335b3f40514f835fb1e130cdeb5c73107fad455a
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:39:20 +0900

    [Gold V] Title: 강의실 배정, Time: 60 ms, Memory: 5188 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+priority_queue<int, vector<int>, greater<int> > pq;
+vector<pair<int,int> > v;
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	int N;
+    cin >> N;
+    for (int i=0;i<N;i++){
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a,b});
+    }
+    sort(v.begin(),v.end());
+    for (int i=0;i<N;i++){
+        int x=v[i].first;
+        int y=v[i].second;
+        if (!pq.empty() && pq.top()<=x)
+			pq.pop();
+        pq.push(y);
+        
+    }
+    cout << pq.size();
+    return 0;
+}
