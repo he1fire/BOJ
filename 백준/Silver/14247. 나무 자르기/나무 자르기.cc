1eb2d6957f4bcb033f69ee9332ee6f225572c415
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 10:56:25 +0900

    [Silver II] Title: 나무 자르기, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, ans;
+vector<ll> v;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i=0;i<N;i++){
+        ll a;
+        cin >> a;
+        ans+=a;
+    }
+    for (int i=0;i<N;i++){
+        ll a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for (int i=0;i<N;i++)
+        ans+=v[i]*i;
+    cout << ans;
+    return 0;
+}
