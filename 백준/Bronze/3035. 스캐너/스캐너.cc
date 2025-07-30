4aaffd518a1fb1569b63c1fb5841c400fae61517
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 12:41:20 +0900

    [Bronze I] Title: 스캐너, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/3035. 스캐너/스캐너.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    int R, C, ZR, ZC;
+    string arr[51];
+    cin >> R >> C >> ZR >> ZC;
+    for (int i=0;i<R;i++)
+    cin >> arr[i];
+    for (int i=0;i<R;i++)
+        {
+        for (int j=0;j<ZR;j++)
+            {
+            for (int k=0;k<C;k++)
+                {
+                for (int l=0;l<ZC;l++)
+                cout << arr[i][k];
+                }
+            cout << "\n";
+            }
+        }
+    return 0;
+    }
