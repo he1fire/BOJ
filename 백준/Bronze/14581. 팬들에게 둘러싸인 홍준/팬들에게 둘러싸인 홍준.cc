6ff41bbd97d2a3716ebf17200a378988f3ae5830
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 14:12:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string N;
+    cin >> N;
+    cout << ":fan::fan::fan:\n" << ":fan::" << N << "::fan:\n" << ":fan::fan::fan:";
+    return 0;
+    }
