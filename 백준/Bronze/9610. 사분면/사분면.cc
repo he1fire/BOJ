35809844407e95d4fe7a34c9646fe3ce4c1a1089
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:16:47 +0900

    [Bronze III] Title: 사분면, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int N, x, y, q1=0, q2=0, q3=0, q4=0, axis=0;
+    cin >> N;
+    for (int i=1;i<=N;i++)
+        {
+        cin >> x >> y;
+        if (x==0 || y==0)
+        axis++;
+        else if (x>0 && y>0)
+        q1++;
+        else if (x<0 && y>0)
+        q2++;
+        else if (x<0 && y<0)
+        q3++;
+        else if (x>0 && y<0)
+        q4++;
+        }
+    cout << "Q1: " << q1 << "\n";
+    cout << "Q2: " << q2 << "\n";
+    cout << "Q3: " << q3 << "\n";
+    cout << "Q4: " << q4 << "\n";
+    cout << "AXIS: " << axis;
+    return 0;
+    }
