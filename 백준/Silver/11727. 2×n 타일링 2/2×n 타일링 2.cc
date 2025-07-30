8512b8d1357154e06c5ae3beadae96a5bdd93121
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:51:47 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int N, x[1001];
+    x[1]=1;
+    x[2]=3;
+    cin >> N;
+    for (int i=3;i<=N;i++)
+    x[i]=(x[i-1]+2*x[i-2])%10007;
+    cout << x[N];
+    return 0;
+    }
