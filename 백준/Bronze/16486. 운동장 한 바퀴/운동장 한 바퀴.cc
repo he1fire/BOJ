77a0a967f14c925f51233a092090738f715eb7d8
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:46:09 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+long double N, M, pi=3.141592;
+int main(){
+    cin >> N >> M;
+    printf("%.6Lf",N*2+2*pi*M);
+    return 0;
+}
