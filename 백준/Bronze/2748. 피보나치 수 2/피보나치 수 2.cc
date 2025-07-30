72cdd175d9a7e89dacd81b9db5b82a6e5ef5b5bb
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 12:28:46 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    long long N, A=0, B=1, C=0;
+    cin >> N;
+    for (int i=1;i<=N;i++)
+        {
+        A=B;
+        B=C;
+        C=A+B;
+        }
+    cout << C;
+    return 0;
+    }
