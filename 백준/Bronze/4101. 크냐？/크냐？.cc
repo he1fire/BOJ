a9e395586621d59026e393b2f887df7f3d386b7b
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 20:42:00 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    while (1)
+        {
+        int A, B;
+        cin >> A >> B;
+        if (A==0 && B==0)
+        break;
+        else if (A>B)
+        cout << "Yes\n";
+        else
+        cout << "No\n";
+        }
+    return 0;
+    }
