d7cfc40528ef0af199b7483ffbdcc317a302e52c
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:18:33 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    long long N;
+    cin >> N;
+    if (N%2==1)
+        {
+        cout << "SK";
+        }
+    else 
+        {
+        cout << "CY";
+        }
+    return 0;
+    }
