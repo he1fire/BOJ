545eb3d0bafa6523c0e7ac5bc7125cfc02e992c5
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 16:36:09 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M, K;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M >> K;
+    cout << M/N*K;
+    return 0;
+}
