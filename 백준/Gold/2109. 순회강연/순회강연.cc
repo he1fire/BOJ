a64643d9a37e6014856a7c98ce04704ec6ffcf07
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 12:03:34 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, ans;
+vector<ll> arr[10005];
+priority_queue<ll> pq;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i=0;i<N;i++){
+        ll a, b;
+        cin >> a >> b;
+        arr[b].push_back(a);
+    }
+    for (int i=10000;i>0;i--){
+        for (auto x:arr[i])
+            pq.push(x);
+        if (!pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+        }
+    }
+    cout << ans;
+    return 0;
+}
