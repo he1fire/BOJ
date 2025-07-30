bd38073fcf7069220c637ee5f2040eba49a8e218
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 12:13:09 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int N=0, M=0;
+    for (int i=1;i<=10;i++)
+        {
+        int A,B;
+        cin >> A >> B;
+        N+=B-A;
+        if (M<N)
+        M=N;
+        }
+    cout << M;
+    return 0;
+    }
