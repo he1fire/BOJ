256d45c85842a14ed7e907475149ce49c69903e8
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 16:14:32 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    while (N--)
+        cout << "a";
+    return 0;
+}
