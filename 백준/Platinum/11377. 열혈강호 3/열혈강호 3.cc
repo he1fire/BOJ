eacefb3125e2904839c5818da274c2540e870154
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 13:45:12 +0900

    [Platinum III] Title: 열혈강호 3, Time: 468 ms, Memory: 10196 KB -BaekjoonHub

---
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M, K, arr[1005], visited[2005];
+vector<ll> v[2005];
+ll DFS(ll x){
+    visited[x]=1;
+    for (auto y:v[x]){
+        if (arr[y]==-1 || (!visited[arr[y]] && DFS(arr[y]))){
+            arr[y]=x;
+            return 1;
+        }
+    }
+    return 0;
+}
+ll BipartiteMatching(){
+    ll ret=0, cnt=0;
+    fill(&arr[0],&arr[1004],-1);
+    for (int i=0;i<N;i++){
+        fill(&visited[0],&visited[2004],0);
+        if (DFS(i*2))
+            ret++;
+    }
+    for (int i=0;i<N;i++){
+        fill(&visited[0],&visited[2004],0);
+        if (DFS(i*2+1))
+            cnt++;
+    }
+    return ret+min(K,cnt);
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M >> K;
+    for (int i=0;i<N;i++){
+        ll a, b;
+        cin >> a;
+        for (int j=0;j<a;j++){
+            cin >> b;
+            v[i*2].push_back(b-1);
+            v[i*2+1].push_back(b-1);
+        }
+    }
+    cout << BipartiteMatching();
+    return 0;
+}
