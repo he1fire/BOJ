e23b42bd03e2ff52c28d377b06ece6752d06492f
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 16:43:35 +0900

    [Unrated] Title: 3분 그래프, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Unrated/17113. 3분 그래프/3분 그래프.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 987654321
+using namespace std;
+typedef pair<ll,ll> pll;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "1";
+    return 0;
+}
