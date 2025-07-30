82efe2a7763ea77708d79a5e02a12d6b69203d61
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 11:40:52 +0900

    [Platinum III] Title: 스위치, Time: 96 ms, Memory: 9024 KB -BaekjoonHub

---
+++ 백준/Platinum/1395. 스위치/스위치.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M, arr[100005];
+vector<ll> seg, lazy;
+void Propagate(int n, int l, int r){
+    if (lazy[n]){
+        if (l!=r){
+            lazy[n*2]^=lazy[n];
+            lazy[n*2+1]^=lazy[n];
+        }
+        seg[n]=(r-l+1)-seg[n];
+        lazy[n]=0;
+    }
+}
+ll Query(int n, int l, int r, int a, int b){
+    Propagate(n,l,r);
+    if (l>b || r<a)
+        return 0;
+    if (l>=a && r<=b)
+        return seg[n];
+    int mid=(l+r)/2;
+    return Query(n*2,l,mid,a,b)+Query(n*2+1,mid+1,r,a,b);
+}
+ll Update(int n, int l, int r, int a, int b, ll x){
+    Propagate(n,l,r);
+    if (l>b || r<a)
+        return seg[n];
+    if (l>=a && r<=b){
+        lazy[n]+=x;
+        Propagate(n,l,r);
+        return seg[n];
+    }
+    int mid=(l+r)/2;
+    return seg[n]=Update(n*2,l,mid,a,b,x)+Update(n*2+1,mid+1,r,a,b,x);
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    seg.resize(4*N);
+    lazy.resize(4*N);
+    for (int i=0;i<M;i++){
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a==0)
+            Update(1,1,N,b,c,1);
+        else
+        cout << Query(1,1,N,b,c) << "\n";
+    }
+    return 0;
+}
