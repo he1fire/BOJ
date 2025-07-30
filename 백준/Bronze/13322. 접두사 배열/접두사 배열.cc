e8124ff1b79338b438183a653d5ef30bd0e66c67
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 14:02:16 +0900

    [Bronze I] Title: 접두사 배열, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+string S;
+int main(){
+    cin >> S;
+    for (int i=0;i<S.size();i++)
+        cout << i << "\n";
+    return 0;
+}
