d2696bebae689aaad65301b274ba00abfd1204e4
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:33:54 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 6392 ms, Memory: 8260 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int N;
+    cin >> N;
+    for (int i=0;i<N;i++){
+        int a;
+        cin >> a;
+        pq.push(a);
+    }
+    for (int i=0;i<N;i++){
+        cout << pq.top() << "\n";
+        pq.pop();
+    }
+    return 0;
+}
