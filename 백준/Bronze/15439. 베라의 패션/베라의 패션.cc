602fc03b5d6a77bcad2a7c99baf2450cf2dc9957
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 15:29:50 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll N;
+    cin >> N;
+    cout << N*N-N;
+    return 0;
+}
