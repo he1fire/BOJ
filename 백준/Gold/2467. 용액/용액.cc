086c231a89ce939615833da0a9f150c2b270f443
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-07-30 12:13:20 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    vector<ll> v;
+    for (ll i=0;i<N;i++){
+        ll a;
+        cin >> a;
+        v.push_back(a);
+    }
+    ll ans=2000000001, i=0, j=N-1, a=0, b=0;
+    while (i<N && j>i){
+        if (ans>abs(v[i]+v[j])){
+            ans=min(ans, abs(v[i]+v[j]));
+            a=v[i];
+            b=v[j];
+        }
+        if (v[i]+v[j]>0)
+            j--;
+        else
+            i++;
+    }
+    cout << a << " " << b;
+    return 0;
+}
