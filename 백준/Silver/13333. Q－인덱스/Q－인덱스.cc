cf704a8b1d2d9aac76e1ff2cfa69483be9853af5
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-08-13 17:42:35 +0900

    [Silver V] Title: Q-인덱스, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/13333. Q－인덱스/Q－인덱스.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, ans;
+vector<ll> v;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i=0;i<N;i++){
+        ll a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for (int i=0;i<N;i++){
+        if (v[i]>=N-i){
+            ans=N-i;
+            break;
+        }
+    }
+    cout << ans;
+    return 0;
+}
