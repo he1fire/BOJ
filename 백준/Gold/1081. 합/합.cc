9fe1992b0118507ed9f937571e8faee8087a4ba7
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-08-14 10:42:29 +0900

    [Gold I] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1081. 합/합.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M;
+ll f(ll num){
+    ll ret=0;
+    vector<ll> v;
+    for (int i=0;i<10;i++){
+        if (i==0)
+            v.push_back(0);
+        else
+            v.push_back(v[i-1]+i);
+    }
+    for (ll i=1;i<=num;i*=10){
+        ll hi=num/(i*10), x=(num%(i*10))/i, lo=num%i;
+        ret+=v[9]*i*hi;
+        if (x)
+            ret+=v[x-1]*i;
+        ret+=x*(lo+1);
+    }
+    return ret;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    cout << f(M)-f(N-1);
+    return 0;
+}
