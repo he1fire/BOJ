fd473934df5fa3d6f5d683765b54b0f9fd05c761
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-11-10 17:32:15 +0900

    [Silver V] Title: 크리 문자열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/11059. 크리 문자열/크리 문자열.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll unsigned long long
+using namespace std;
+string S;
+ll arr[1005], ans;
+void f(ll len){
+    for (int i=1;i+len-1<=S.size();i++){
+        if (arr[i+len/2-1]-arr[i-1]==arr[i+len-1]-arr[i+len/2-1])
+            ans=len;
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> S;
+    for (int i=1;i<=S.size();i++)
+        arr[i]=S[i-1]-'0'+arr[i-1];
+    for (int i=1;i*2<=S.size();i++)
+        f(i*2);
+    cout << ans;
+    return 0;
+}
