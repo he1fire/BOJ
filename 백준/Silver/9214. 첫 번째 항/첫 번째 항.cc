6fcfa3057cc4880a0498e8bf25192a620a3fbaf7
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-11-11 17:11:14 +0900

    [Silver IV] Title: 첫 번째 항, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9214. 첫 번째 항/첫 번째 항.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll unsigned long long
+using namespace std;
+string S;
+string f(string s){
+    if (s.size()%2)
+        return "";
+    string ret="";
+    for (int i=0;i<s.size();i+=2){
+        ll cnt=s[i]-'0';
+        while (cnt--)
+            ret+=s[i+1];
+    }
+    return ret;
+}
+string g(string s){
+    if (s=="")
+        return "";
+    string ret="";
+    ll cnt=1;
+    char c=s[0];
+    for (int i=1;i<s.size();i++){
+        if (s[i]==c)
+            cnt++;
+        else{
+            ret+=(char)(cnt+'0');
+            ret+=c;
+            c=s[i];
+            cnt=1;
+        }
+    }
+    ret+=(char)(cnt+'0');
+    ret+=c;
+    return ret;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    for (int i=1;1;i++){
+        cin >> S;
+        if (S=="0")
+            break;
+        else{
+            while (S==g(f(S)) && S!=f(S))
+                S=f(S);
+            cout << "Test "<< i << ": " << S << "\n";
+        }
+    }
+    return 0;
+}
