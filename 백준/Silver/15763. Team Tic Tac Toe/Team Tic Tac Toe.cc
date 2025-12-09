085a16ad8fa4ed7d893a97f5126471fa70bb5d8d
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-12-09 14:50:46 +0900

    [Silver IV] Title: Team Tic Tac Toe, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15763. Team Tic Tac Toe/Team Tic Tac Toe.cc
+++ 백준/Silver/15763. Team Tic Tac Toe/Team Tic Tac Toe.cc
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-ll ans[2], arr[8][3]={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+ll arr[8][3]={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+set<char> s1;
+set<string> s2;
 string S;
 int main(){
     ios::sync_with_stdio(0);
@@ -11,37 +13,19 @@ int main(){
         cin >> a;
         S+=a;
     }
-    for (char x='A';x<='Z';x++){
-        ll cnt[2]={0,};
-        for (ll i=0;i<8;i++){
-            ll a=arr[i][0], b=arr[i][1], c=arr[i][2];
-            if (S[a]==x && S[b]==x && S[c]==x)
-                cnt[0]++;
-        }
-        ans[0]+=(cnt[0]!=0);
-        for (char y=x+1;y<='Z';y++){
-            cnt[1]=0;
-            if (x==y)
-                continue;
-            for (ll i=0;i<8;i++){
-                ll a=arr[i][0], b=arr[i][1], c=arr[i][2];
-                if (S[a]==x && S[b]==x && S[c]==y)
-                    cnt[1]++;
-                if (S[a]==x && S[b]==y && S[c]==x)
-                    cnt[1]++;
-                if (S[a]==y && S[b]==x && S[c]==x)
-                    cnt[1]++;
-                if (S[a]==x && S[b]==y && S[c]==y)
-                    cnt[1]++;
-                if (S[a]==y && S[b]==x && S[c]==y)
-                    cnt[1]++;
-                if (S[a]==y && S[b]==y && S[c]==x)
-                    cnt[1]++;
-            }
-            ans[1]+=(cnt[1]!=0);
+    for (ll i=0;i<8;i++){
+        set<char> tmp;
+        for (ll j=0;j<3;j++)
+            tmp.insert(S[arr[i][j]]);
+        if (tmp.size()==1)
+            s1.insert(*tmp.begin());
+        if (tmp.size()==2){
+            string s="";
+            for (auto c:tmp)
+                s+=c;
+            s2.insert(s);
         }
     }
-    for (auto x:ans)
-        cout << x << "\n";
+    cout << s1.size() << "\n" << s2.size();
     return 0;
 }
