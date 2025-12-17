ae3fcaad03b3256059bcce72547bf72de876069e
moongi choe <34001115+he1fire@users.noreply.github.com>
2025-12-17 17:21:29 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+string S;
+ll ans;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while (getline(cin,S))
+        ans++;
+    cout << ans;
+    return 0;
+}
