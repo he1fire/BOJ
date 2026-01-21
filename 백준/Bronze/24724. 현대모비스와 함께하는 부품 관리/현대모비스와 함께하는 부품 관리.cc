d7116051f1b2537a21e655d864c0b692523e382d
moongi choe <34001115+he1fire@users.noreply.github.com>
2026-01-21 11:25:53 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N, M;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (ll i=1;i<=N;i++)
+        cout << "Material Management " << i << "\nClassification ---- End!\n";
+    return 0;
+}
