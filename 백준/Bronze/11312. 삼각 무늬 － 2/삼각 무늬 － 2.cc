83de7754e7ae35aa79eefd1b1d7ec21422bab448
moongi choe <34001115+he1fire@users.noreply.github.com>
2026-02-15 02:04:05 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    while (N--){
+        ll a, b;
+        cin >> a >> b;
+        cout << a/b*a/b << "\n";
+    }
+    return 0;
+}
